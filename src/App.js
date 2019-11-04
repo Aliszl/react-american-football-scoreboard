1c3831280c94edc762c295faa2e16440aed27d5c
Alison Law <alisparklaw@gmail.com>
2019-11-04 18:22:30 +0000

    reset and remove buttons

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ function App() {
   
 const [lionsScore, setLionsScore] = useState(0);
 const [tigersScore, setTigersScore] = useState(0);
-// console.log(this.useState.home);
+
+// const [timeleft, setTimeLeft]= useState(60);
+
   return (
     
   
@@ -27,7 +29,7 @@ const [tigersScore, setTigersScore] = useState(0);
 
             <div className="home__score">{lionsScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">00.03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{tigersScore}</div>
@@ -40,14 +42,53 @@ const [tigersScore, setTigersScore] = useState(0);
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button className="homeButtons__touchdown" onClick = {() => setLionsScore(lionsScore+7)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal"onClick = {() => setLionsScore(lionsScore+14)}>Home Field Goal</button>
+          <button className="homeButtons__fieldGoal"onClick = {() => setLionsScore(lionsScore-7)}>remove Home Touchdown</button>
+          <button className="homeButtons__fieldGoal"onClick = {() => setLionsScore(lionsScore-14)}>remove Home Field goal</button>
+          <button className="homeButtons__reset" onClick={() => setLionsScore(0)} >Lions Reset</button>
         </div>
         <div className="awayButtons">
           <button className="awayButtons__touchdown"onClick = {() => setTigersScore(tigersScore+7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal"onClick = {() => setTigersScore(tigersScore+14)}>Away Field Goal</button>
+          <button className="awayButtons__fieldGoal"onClick = {() => setTigersScore(tigersScore-7)}>remove away Touchdown</button>
+          <button className="awayButtons__touchdown"onClick = {() => setTigersScore(tigersScore-14)}>remove away field goal</button>
+          <button className="awayButtons__reset" onClick={() => setTigersScore(0)} >Tigers Reset</button>
         </div>
+       
+        
+
+        
       </section>
     </div>
   );
 }
 
 export default App;
+
+
+// const Timer = ({ seconds }) => {
+//   // initialize timeLeft with the seconds prop
+//   const [timeLeft, setTimeLeft] = useState(seconds);
+
+//   useEffect(() => {
+//     // exit early when we reach 0
+//     if (!timeLeft) return;
+
+//     // save intervalId to clear the interval when the
+//     // component re-renders
+//     const intervalId = setInterval(() => {
+//       setTimeLeft(timeLeft - 1);
+//     }, 1000);
+
+//     // clear interval on re-render to avoid memory leaks
+//     return () => clearInterval(intervalId);
+//     // add timeLeft as a dependency to re-rerun the effect
+//     // when we update it
+//   }, [timeLeft]);
+
+//   return (
+//     <div>
+//       <h1>{timeLeft}</h1>
+//     </div>
+//   );
+// };
+// onClick= "setTimeout('alert('first quarter over?')', 1000)" >{timeleft}
